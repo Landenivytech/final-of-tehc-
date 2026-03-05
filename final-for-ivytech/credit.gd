69018cb29e44c4c499816504cb08b938f4131f19
Landenivytech <ltillman16@ivytech.edu>
2026-03-04 22:52:05 -0500

    Add files via upload

---
+++ final-for-ivytech/credit.gd
@@ -0,0 +1,4 @@
+#this is the stuff i used and heres credit
+#https://opengameart.org/content/ww2-soldier
+#https://opengameart.org/content/cannonball
+#https://opengameart.org/content/battle-in-the-winter
